2d06e203ae0ba14b31f862b9589f83b49fce7411
Hayden Pennington <haydenpennington@icloud.com>
2020-07-04 22:01:07 -0500

    Update marketing site

--- marketing/src/pages/index.js
+++ marketing/src/pages/index.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Kanception from "../images/kanception.png"
 import "./index.css"
 
 const IndexPage = () => (
@@ -10,6 +11,13 @@ const IndexPage = () => (
     <SEO title="Kanban Home" />
     <h1>A recursive Kanban board</h1>
     <a className="sign-up-btn" href="https://app.kanception.io">Sign In / Sign Up</a>
+    <img style={{
+        boxShadow: "0 0 5px 10px #ccc",
+        margin: "40px auto",
+        borderRadius: "5px"
+      }}
+      src={Kanception}
+    />
   </Layout>
 )
 
