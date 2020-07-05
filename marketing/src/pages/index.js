e4eb20906b8a47477124e47c5b2f9eee905e717b
Hayden Pennington <haydenpennington@icloud.com>
2020-07-04 21:08:21 -0500

    Update marketing page to basic kanception

--- marketing/src/pages/index.js
+++ marketing/src/pages/index.js
@@ -2,20 +2,14 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import "./index.css"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <SEO title="Kanban Home" />
+    <h1>A recursive Kanban board</h1>
+    <a className="sign-up-btn" href="https://app.kanception.io">Sign In / Sign Up</a>
   </Layout>
 )
 
