4eef2ba569b977d9522a07e6c7295cc230370436
Hayden Pennington <haydenpennington@icloud.com>
2020-07-08 18:07:36 -0500

    Fix "typo"

--- marketing/src/pages/index.js
+++ marketing/src/pages/index.js
@@ -7,7 +7,7 @@ import Kanception from "../images/kanception.png"
 import Demo from './demo.mp4'
 import "./index.css"
 
-const IndexPage = () => (
+const IndexPage = () => {
   const layout = useRef(null)
   const [width, setWidth] = useState(900)
 
@@ -18,25 +18,27 @@ const IndexPage = () => (
     }
   })
 
-  <Layout ref={layout}>
-    <SEO title="Kanban Home" />
-    <h1 className="center-text mid-width">A nested Kanban board for creative professionals</h1>
-    <a className="sign-up-btn" href="https://app.kanception.io">Sign In / Sign Up</a>
-    <video
-      autoPlay={true}
-      loop={true}
-      controls={true}
-      muted={true}
-      width={width}
-      style={{
-        boxShadow: "0 0 5px 10px #ccc",
-        margin: "40px auto",
-        borderRadius: "5px"
-      }}
-    >
-      <source src={Demo} type="video/mp4" />
-    </video>
-  </Layout>
-)
+  return (
+    <Layout ref={layout}>
+      <SEO title="Kanban Home" />
+      <h1 className="center-text mid-width">A nested Kanban board for creative professionals</h1>
+      <a className="sign-up-btn" href="https://app.kanception.io">Sign In / Sign Up</a>
+      <video
+        autoPlay={true}
+        loop={true}
+        controls={true}
+        muted={true}
+        width={width}
+        style={{
+          boxShadow: "0 0 5px 10px #ccc",
+          margin: "40px auto",
+          borderRadius: "5px"
+        }}
+      >
+        <source src={Demo} type="video/mp4" />
+      </video>
+    </Layout>
+  )
+}
 
 export default IndexPage
