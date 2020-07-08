3436b59f2a58efa25a7d1d34ee01451ede3898fb
Hayden Pennington <haydenpennington@icloud.com>
2020-07-08 13:36:33 -0500

    Marketing: Add video to website

--- marketing/src/pages/index.js
+++ marketing/src/pages/index.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Kanception from "../images/kanception.png"
+import Demo from './demo.mp4'
 import "./index.css"
 
 const IndexPage = () => (
@@ -11,13 +12,18 @@ const IndexPage = () => (
     <SEO title="Kanban Home" />
     <h1 className="center-text mid-width">A nested Kanban board for creative professionals</h1>
     <a className="sign-up-btn" href="https://app.kanception.io">Sign In / Sign Up</a>
-    <img style={{
+    <video
+      autoPlay={true}
+      loop={true}
+      width={900}
+      style={{
         boxShadow: "0 0 5px 10px #ccc",
         margin: "40px auto",
         borderRadius: "5px"
       }}
-      src={Kanception}
-    />
+    >
+      <source src={Demo} type="video/mp4" />
+    </video>
   </Layout>
 )
 
