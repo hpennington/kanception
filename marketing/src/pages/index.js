2992bef70e5155f04d7cdc969e639e3a82ffc3bc
Hayden Pennington <haydenpennington@icloud.com>
2020-07-08 18:05:45 -0500

    Fix video width

--- marketing/src/pages/index.js
+++ marketing/src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useRef, useEffect, useState } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
@@ -8,7 +8,17 @@ import Demo from './demo.mp4'
 import "./index.css"
 
 const IndexPage = () => (
-  <Layout>
+  const layout = useRef(null)
+  const [width, setWidth] = useState(900)
+
+  useEffect(() => {
+    const width = ref.current.getBoundingClientRect().width
+    if (width <= 900) {
+      setWidth(width * 0.8)
+    }
+  })
+
+  <Layout ref={layout}>
     <SEO title="Kanban Home" />
     <h1 className="center-text mid-width">A nested Kanban board for creative professionals</h1>
     <a className="sign-up-btn" href="https://app.kanception.io">Sign In / Sign Up</a>
@@ -17,7 +27,7 @@ const IndexPage = () => (
       loop={true}
       controls={true}
       muted={true}
-      width={900}
+      width={width}
       style={{
         boxShadow: "0 0 5px 10px #ccc",
         margin: "40px auto",
