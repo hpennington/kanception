3b065d3de0d1584274744223f009305fe2f9a67f
Hayden Pennington <haydenpennington@icloud.com>
2020-07-12 23:17:57 -0500

    Update Title for SEO

--- marketing/src/pages/index.js
+++ marketing/src/pages/index.js
@@ -12,7 +12,7 @@ import "./index.css"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Kanban Home" />
+    <SEO title="Kanban Project Management Tool" />
     <MainVideoBlock />
     <KanbanDetailBlock />
     <GanttBlock />
