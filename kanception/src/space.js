7bf2f067513f1b5cac4c0e494702ed85632075d3
Hayden Pennington <haydenpennington@icloud.com>
2020-07-26 15:27:48 -0500

    Update copy in app

--- kanception/src/space.js
+++ kanception/src/space.js
@@ -4,7 +4,7 @@ import './space.css'
 const Space = props => (
   <div className="space">
     <h1>{props.title}</h1>
-    <h2>Create a new board to get started</h2>
+    <h2>Create a new project to get started</h2>
     <button
       onClick={props.onNewProject}
       className="new-project"
