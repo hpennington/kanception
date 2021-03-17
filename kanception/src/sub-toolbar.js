2408ee7bbf8ddf93fdf1b631cef39c77a9c8a874
Hayden Pennington <haydenpennington@icloud.com>
2021-03-16 23:43:27 -0500

    Remove comments view

--- kanception/src/sub-toolbar.js
+++ kanception/src/sub-toolbar.js
@@ -4,7 +4,7 @@ import CommentIcon from './features/kanban/icon-comment.png'
 const SubToolbar = props => {
   return (
     <div style={{marginTop: "60px", color: "dodgerblue", display: "flex", position: "fixed"}}>
-      <img onClick={props.onCommentsClick} src={CommentIcon} width={"36px"} style={{margin: "10px", objectFit: "scale-down"}}></img>
+      {/*<img onClick={props.onCommentsClick} src={CommentIcon} width={"36px"} style={{margin: "10px", objectFit: "scale-down"}}></img>*/}
       <h6 style={{margin: "10px"}}>{props.title}</h6>
     </div>
   )
