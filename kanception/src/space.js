3117a34ec7292c87b9805eed6b96a06d1d9de6d9
Hayden Pennington <haydenpennington@icloud.com>
2021-03-17 00:11:50 -0500

    Update space page

--- kanception/src/space.js
+++ kanception/src/space.js
@@ -3,16 +3,18 @@ import './space.css'
 
 const Space = props => (
   <div className="space">
+    <div className="btn-container">
+      <button
+        onClick={props.onNewProject}
+        className="new-project"
+      >New Project</button>
+      <button
+        onClick={props.onDeleteSpace}
+        className="delete-space"
+      >Delete Space</button>
+    </div>
     <h1>{props.title}</h1>
     <h2>Create a new project to get started</h2>
-    <button
-      onClick={props.onNewProject}
-      className="new-project"
-    >New Project</button>
-    <button
-      onClick={props.onDeleteSpace}
-      className="delete-space"
-    >Delete Space</button>
   </div>
 )
 
