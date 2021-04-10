4f12996b76f8319a6a1ea789eab3ae1feea1de86
Hayden Pennington <haydenpennington@icloud.com>
2021-04-09 22:07:11 -0500

    Fix shit

--- api/src/app/models/sequelize/index.js
+++ api/src/app/models/sequelize/index.js
@@ -18,7 +18,7 @@ if (config.use_env_variable) {
 fs
   .readdirSync(path.join(__dirname, '../src/app/models/sequelize'))
   .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    return (file.indexOf('.') !== 0) && (file !== 'index.js') && (file.slice(-3) === '.js');
   })
   .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
