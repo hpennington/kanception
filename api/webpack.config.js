e96510eb94ab57505814e0006e1ce14af014dcc4
Hayden Pennington <haydenpennington@icloud.com>
2021-04-09 21:28:50 -0500

    Update webpack config

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -4,6 +4,7 @@ const nodeExternals = require('webpack-node-externals');
 module.exports = {
   target: 'node',
   entry: './src/index.ts',
+  devtool: 'source-map',
   module: {
     rules: [
       {
